a0ccef73c0a9abc5f29f321756775bffd12adc76
Marc Sanchez <marc.sanchez.civantos@gmail.com>
2022-04-09 23:41:57 +0200

    fix warnings

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import style from './app.module.css';
 import Calendar from "./components/Calendar/Calendar"
 import {useState} from "react"
-import Input from './components/Input/Input';
-import Box from './components/Box/Box';
 import Header from './components/Header/Header';
 
 function App() {
@@ -10,7 +8,8 @@ function App() {
   const [year, setYear] = useState(date.slice(0,4));
   const [month, setMonth] = useState(date.slice(5,7));
   const [day, setDay] = useState(date.slice(8,10));
-  const [type, setType] = useState('1');
+  const type = "1";
+  //const [type, setType] = useState('1');
   return (
     <div className={style.app}>
       <Header year = {year} setYear={setYear} month={month} setMonth = {setMonth} day = {day} setDay = {setDay}/>
